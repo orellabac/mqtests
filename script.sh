9c90ff40b5d88063f9c7d4414325f48c53022e21
Orellabac <orellabac@gmail.com>
2023-10-27 06:11:49 +0000

    files

--- script.sh
+++ script.sh
@@ -1,12 +1,25 @@
-docker pull icr.io/ibm-messaging/mq:latest
-docker images
-docker volume create qm1data
-docker run --env LICENSE=accept --env MQ_QMGR_NAME=c --volume qm1data:/mnt/mqm --publish 1414:1414 --publish 9443:9443 --detach --env MQ_APP_PASSWORD=passw0rd --name QM1 icr.io/ibm-messaging/mq:latest
+# this script creates a docker container to test mq usage
+# https://developer.ibm.com/tutorials/mq-connect-app-queue-manager-containers/
 
+# pulls the image
+docker pull icr.io/ibm-messaging/mq:latest
+# displays current images
+docker images
+# creates a volume
+docker volume create qm1data
+#starts the docker container
+docker run --env LICENSE=accept --env MQ_QMGR_NAME=QM1 --volume qm1data:/mnt/mqm --publish 1414:1414 --publish 9443:9443 --detach --env MQ_APP_PASSWORD=passw0rd --name QM1 icr.io/ibm-messaging/mq:latest
+# display the current docker info
 docker ps
 
-#docker exec -ti QM1 bash
-#dspmqver
-#dspmq
+#to connect to the container
+# docker exec -ti QM1 bash
 
-amqsbcgc 
+#this command can be used to display info: dspmqver
+#this command shows the queue: dspmq
+
+# this command can be used to get the public URL
+gp url 1414 
+
+# connecting to queue
+runmqsc QM1
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,29 +9,24 @@ import com.ibm.mq.constants.CMQC;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void sendMessage(
+            String mqHost, String mqPort,
+            String mqChannel, String mqQMgr, String mqQueue,
+            String userID, String password, String message_str) {
         try {
-
-            String mqHost = "localhost"; // Hostname
-            String mqPort = "1414"; // Port
-            String mqChannel = "DEV.APP.SVRCONN"; // Channel 
-            String mqQMgr = null;   // Queue Manager
-            MQQueueManager qMgr = null;
             MQEnvironment.hostname = mqHost;
             MQEnvironment.port = Integer.valueOf(mqPort).intValue();
             MQEnvironment.channel = mqChannel;
             MQEnvironment.properties.put(MQC.TRANSPORT_PROPERTY, MQC.TRANSPORT_MQSERIES);
-            MQEnvironment.userID = "app";
-            MQEnvironment.password = "passw0rd";
-            //java.util.Hashtable props = new java.util.Hashtable();
-            //props.put(MQC.TRANSPORT_PROPERTY, MQC.TRANSPORT_MQSERIES_CLIENT);            
-            MQQueueManager qm = new MQQueueManager("QM1");
-            MQQueue outputQueue = qm.accessQueue("DEV.QUEUE.1", CMQC.MQOO_OUTPUT);
+            MQEnvironment.userID = userID;
+            MQEnvironment.password = password;
+            MQQueueManager qm = new MQQueueManager(mqQMgr);
+            MQQueue outputQueue = qm.accessQueue(mqQueue, CMQC.MQOO_OUTPUT);
             MQMessage message = new MQMessage();
-            message.writeUTF("This is a test message");
+            message.writeUTF(message_str);
             outputQueue.put(message);
             outputQueue.close();
-            MQQueue inputQueue = qm.accessQueue("DEV.QUEUE.1", CMQC.MQOO_INPUT_SHARED);
+            MQQueue inputQueue = qm.accessQueue(mqQueue, CMQC.MQOO_INPUT_SHARED);
             MQMessage readMessage = new MQMessage();
             inputQueue.get(readMessage);
             inputQueue.close();
@@ -42,5 +37,43 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static String readMessage(
+            String mqHost, String mqPort,
+            String mqChannel, String mqQMgr, String mqQueue,
+            String userID, String password, String message_str) {
+        try {
+            MQEnvironment.hostname = mqHost;
+            MQEnvironment.port = Integer.valueOf(mqPort).intValue();
+            MQEnvironment.channel = mqChannel;
+            MQEnvironment.properties.put(MQC.TRANSPORT_PROPERTY, MQC.TRANSPORT_MQSERIES);
+            MQEnvironment.userID = userID;
+            MQEnvironment.password = password;
+            MQQueueManager qm = new MQQueueManager(mqQMgr);
+
+            MQQueue inputQueue = qm.accessQueue(mqQueue, CMQC.MQOO_INPUT_SHARED);
+            MQMessage readMessage = new MQMessage();
+            inputQueue.get(readMessage);
+            inputQueue.close();
+            qm.disconnect();
+            return readMessage.readUTF();
+        } catch (MQException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String mqHost = "4.tcp.us-cal-1.ngrok.io";//"localhost"; // Hostname
+        String mqPort = "16800";//"1414"; // Port
+        String mqChannel = "DEV.APP.SVRCONN"; // Channel
+        String mqQMgr = "QM1"; // Queue Manager
+        String mqQueue = "DEV.QUEUE.1";// Queue
+        String user = "app";
+        String pass = "passw0rd";
+        sendMessage(mqHost, mqPort, mqChannel, mqQMgr, mqQueue, user, pass, "mymessage");
     }
 }
